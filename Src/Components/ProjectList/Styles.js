52a081a235dced277d3d562560cfdd7d73871707
Alin Ali Hassan <hassanalinali@gmail.com>
2021-06-19 13:50:26 +0200

    Lint fix

--- Src/Components/ProjectList/Styles.js
+++ Src/Components/ProjectList/Styles.js
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.2,
     shadowRadius: 1.41,
     elevation: 2,
-    marginBottom: 20
+    marginBottom: 20,
   },
   mainImage: {
     width: '100%',
